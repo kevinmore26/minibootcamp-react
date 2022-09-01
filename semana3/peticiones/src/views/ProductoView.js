5ed630e13ba6bcc413e5f0c1afa19d97b678505e
Kevin More <74431425+kevinmore26@users.noreply.github.com>
2022-08-31 21:14:43 -0500

    commit

--- semana3/peticiones/src/views/ProductoView.js
+++ semana3/peticiones/src/views/ProductoView.js
@@ -2,9 +2,25 @@ import {useState, useEffect, } from "react"
 import { useParams } from "react-router-dom"
 //el useParams de react router dom sirve para obtener acceso a los datos de tu URL
 //gracias al useParams me permitirá devolver el ID
-
+import {obtenerProductoPorId} from "../services/productosServices"
 export default function ProductoView(){
+    const[producto,setProducto] = useState([])
     
+    const {id} = useParams()
+    const getProducto = async () =>{
+
+        try{
+            let productoObtenido = await obtenerProductoPorId(id)
+            console.log(productoObtenido)
+            setProducto(productoObtenido)
+        }catch{
+                console.log("error")
+        }
+    }
+    useEffect(() => {
+        getProducto()
+      }, []);
+
     return(
         <div>
 
